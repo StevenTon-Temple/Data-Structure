774b6f7c892ac5c6df58f1b60dd6594b8b99b2f6
steve <tun32796@temple.edu>
2022-03-21 10:10:56 -0400

    updated

--- src/main/java/com/example/tree/IndexTree.java
+++ src/main/java/com/example/tree/IndexTree.java
@@ -52,45 +52,197 @@ public class IndexTree{
 			return root;
 		}
 	}
-	
-	
-	
-	
+
+
+
+
 	// returns true if the word is in the index
 	public boolean contains(String word){
-		return true;
+
+
+
+		return contains(this.root, word);
 	}
-	
+
+	private boolean contains(String word){
+
+		if(root == null){
+			return false;
+		}
+		int comparison = word.compareTo(root.word);
+
+		if(comparison == 0){
+			return true;
+		}
+
+		else if(comparison < 0){
+			return contains(root.left, word);
+		}
+
+		else{
+			return contains(root.right, word);
+		}
+	}
+
 	// call your recursive method
 	// use book as guide
 	public void delete(String word){
-		
+
+		this.root = this.delete(this.root, word);
+		size--;
 	}
-	
+
 	// your recursive case
 	// remove the word and all the entries for the word
-	// This should be no different than the regular technique.
+	// This should be no different then the regular technique.
 	private IndexNode delete(IndexNode root, String word){
-		return null;
+
+		//If root is empty then return null
+		if(root == null){
+			return null;
+		}
+		int comparison = word.compareTo(root.word);
+
+		//If word is less than the root, remove the left leaf
+		if(comparison < 0){
+			root.left = delete(root.left, word);
+			return root;
+		}
+		//If word is greater than the root, remove right leaf
+		else if(comparison > 0){
+			root.right = delete(root.right, word);
+			return root;
+		}
+		//Checks if node has any children
+		else{
+
+			//If node has no children then it deletes the node
+			if(root.left == null && root.right == null){
+				return null;
+			}
+
+			//If node has child on the LEFT then has its parent node adopt its child
+			//Doing this changes the pointers and has java garbage collector delete the node
+			else if(root.left != null && root.right == null){
+				return root.left;
+			}
+
+			//If node has child on the RIGHT then has its parent node adopt its child
+			//Doing this changes the pointers and has java garbage collector delete the node
+			else if(root.left == null && root.right != null){
+				return root.right;
+			}
+
+			else{
+				IndexNode current = root.left;
+				while(current.right != null){
+					current = current.right;
+				}
+				root.word = current.word;
+				root.left = delete(root.left, root.word);
+				return root;
+			}
+		}
 	}
-	
-	
+
+
 	// prints all the words in the index in inorder order
 	// To successfully print it out
 	// this should print out each word followed by the number of occurrences and the list of all occurrences
 	// each word and its data gets its own line
 	public void printIndex(){
-		
+
 	}
-	
+
 	public static void main(String[] args){
+
 		IndexTree index = new IndexTree();
+
+		//Example from video. Probably not needed
+		index.add(15);
+		index.add(50);
+		index.add(20);
+		index.add(10);
+		index.add(13);
+		index.add(12);
+		index.add(7);
+		index.add(6);
+
+		System.out.println(index);
+		//Example from video END
+
+
+
+
+
+
 		// add all the words to the tree
-		
+
 		// print out the index
-		
+
 		// test removing a word from the index
 
-		
+
 	}
+}
+
+
+IndexNode:
+
+		import java.util.List;
+
+public class IndexNode  {
+
+	// The word for this entry
+	String word;
+	// The number of occurrences for this word
+	int occurences;
+	// A list of line numbers for this word.
+	List<Integer> list;
+
+
+
+	IndexNode left;
+	IndexNode right;
+
+
+
+
+	// Constructors
+	// Constructor should take in a word and a line number
+	// it should initialize the list and set occurrences to 1
+
+	public IndexNode(String word, int lineNumber) {
+
+	}
+
+
+
+
+	// Complete This
+	// return the word, the number of occurrences, and the lines it appears on.
+	// string must be one line
+
+	public String toString(){
+
+		return toString(this.root);
+	}
+
+	private String toString(Node<E> root){
+
+		if(root == null){
+			return "";
+		}
+		StringBuilder builder = new StringBuilder();
+		builder.append(toString(root.left));
+		builder.append(" ");
+		builder.append(root.item);
+		builder.append(" ");
+		builder.append(toString(root.right));
+
+		return builder.toString();
+	}
+
+
+
 }
